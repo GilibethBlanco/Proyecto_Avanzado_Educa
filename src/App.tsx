780350cafac801d741ed2de23d1dd27f1adf86fe
Gilibeth Blanco <gilibethblanco@gmail.com>
2025-11-15 11:58:14 +0000

    actualización del proyecto
    props de la carta

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import Carta from './Componentes/Carta';
 function App() {
   return (
    <div>
-    <Carta numero={1}/>
-    <Carta numero={2}/>
-    <Carta numero={3}/>
-    <Carta numero={"K"}/>
-    <Carta numero={"J"}/>
-    <Carta numero={"Q"}/>
+    <Carta 
+    numero={1}
+    ataque={222}
+    nombre="malefica" 
+    defensa={100} 
+    descripcion="villana de Disney"
+    imagen="https://m.media-amazon.com/images/M/MV5BMjAwOTk2MGItZTlmYS00Yzk5LTgyNTYtMjkxY2FkZWI1NzcxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" 
+    tipo= "magico"
+    />
    </div>
   );
 }
 
 export default App;
+
--- src/Componentes/Carta.tsx
+++ src/Componentes/Carta.tsx
@@ -1,7 +1,43 @@
 import './Carta.css'
 
-function Carta ({ numero }:{numero: number | string}){
-    return <div className="contenedor">{numero}</div>;
+type props ={
+    numero: number;
+    nombre: string;
+    tipo: string;
+    ataque?: number;
+    defensa: number;
+    descripcion: string;
+    imagen: string;
+
+};
+
+function Carta({
+    ataque=0,
+    defensa,
+    descripcion,
+    imagen,
+    nombre,
+    numero,
+    tipo,
+}: props ) {
+    return(
+  <div>
+    <h3>
+     {nombre} (#{numero})
+    </h3>
+    <img src={imagen} alt={nombre}/>
+    <p>tipo: {tipo} </p>
+    <p>ataque: {ataque} </p>
+    <p>defensa: {defensa} </p>
+    <p>{descripcion} </p>
+  </div>
+    );
 }
 
+
 export default Carta;
+
+
+
+
+
