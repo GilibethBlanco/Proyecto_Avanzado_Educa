2b612dd2720068a6607f4091abfbca606df36fc5
Gilibeth Blanco <gilibethblanco@gmail.com>
2025-11-29 22:16:40 +0000

    actualización de la información de la carta

--- src/Pantallas/VistaMazo.tsx
+++ src/Pantallas/VistaMazo.tsx
@@ -15,8 +15,8 @@ export default function VistaMazo() {
     nombre="Capitán América"
     imagen="https://cuadrosyretablos.com/wp-content/uploads/2022/08/cuadro-decorativo-universo-marvel-capitan-america.jpg"
     ataque={80}
-    defensa={90}
-    vida={100}
+    defensa={80}
+    vida={90}
     onSeleccionar={setCartaSeleccionada}
    />
 
@@ -26,7 +26,7 @@ export default function VistaMazo() {
    imagen="https://i.pinimg.com/736x/1f/19/0b/1f190b0e9b4f44cf99777ea25c48a159.jpg"
    ataque={80}
    defensa={90}
-   vida={100}
+   vida={85}
    onSeleccionar={setCartaSeleccionada}
   />
 
@@ -37,8 +37,8 @@ export default function VistaMazo() {
    nombre="Black Widow"
    imagen="https://4kwallpapers.com/images/wallpapers/black-widow-scarlett-johansson-black-background-2020-movies-2560x2560-2705.jpg"
    ataque={80}
-   defensa={90}
-   vida={100}
+   defensa={70}
+   vida={65}
    onSeleccionar={setCartaSeleccionada}
   />
 
@@ -46,8 +46,8 @@ export default function VistaMazo() {
    numero={4}
    nombre="Bruja Escarlata"
    imagen="https://www.smashmexico.com.mx/wp-content/uploads/2020/11/Avengers-Bruja-Escarlata-Scarlet-Witch-Historia-de-Marvel-poster.jpg"
-   ataque={80}
-   defensa={90}
+   ataque={90}
+   defensa={80}
    vida={100}
    onSeleccionar={setCartaSeleccionada}
   />
@@ -56,9 +56,9 @@ export default function VistaMazo() {
    numero={5}
    nombre="Spider-Man"
    imagen="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2025%2F02%2F24%2Ftom-holland-spider-man-4-receives-delayed-release-july-2026-date-001.jpg?q=75&w=800&cbr=1&fit=max.jpg"
-   ataque={80}
-   defensa={90}
-   vida={100}
+   ataque={70}
+   defensa={60}
+   vida={70}
    onSeleccionar={setCartaSeleccionada}
   />
       
